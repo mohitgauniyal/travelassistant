587579cc07a27a4a1f35678d6346561328931948
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-05-01 19:04:40 +0530

    Add Dynamic data in Albums and Album pages

--- albums/views.py
+++ albums/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-
+from .models import Album
+from .models import Photo
 
 def albums(request):
-    return render(request, 'albums/albums.html')
+    albums = Album.objects.all()
 
-def album(request):
-    return render(request, 'albums/album.html')
+    context = {
+        'albums' : albums
+    }
+
+    return render(request, 'albums/albums.html', context)
+
+
+def album(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    photos = Photo.objects.filter(album=album.id)
+
+    context = {
+        'album' : album,
+        'photos' : photos
+    }
+
+    return render(request, 'albums/album.html', context)
 
 
 '''
