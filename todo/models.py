93f2d1c0bb5f551d465025177773596111087a06
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-05-02 23:11:06 +0530

    Add 'add task' View

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.user.username
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from django.contrib.auth.models import User
 
 def todo(request):
 
     if request.user.is_authenticated:
-        tasks = Task.objects.filter(user=request.user.id)
+        tasks = Task.objects.order_by('-date_added').filter(user=request.user.id)
 
     context = {
         'tasks' : tasks
     }
     return render(request, 'todo/todo.html', context)
+
+
+def add(request):
+
+    if request.method == 'GET':
+        data = request.GET['task']
+
+        if request.user.is_authenticated:
+            id = request.GET['user_id']
+            task = Task(user=request.user, description=data)        
+            task.save()
+            return redirect('todo')
+    else:
+        return render(request, 'todo/todo.html',)
