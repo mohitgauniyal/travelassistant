14da79122144eee6e829de323a2c77e1a1149919
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-05-02 19:21:47 +0530

    Add Login, Register Forms and Messages

--- travelassistant/urls.py
+++ travelassistant/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('realtime.urls')),
     path('todo/', include('todo.urls')),
     path('albums/', include('albums.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,1 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'users/register.html')
+    
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+
+def logout(request):
+    return redirect(request, 'index')
