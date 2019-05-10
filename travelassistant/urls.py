280d35a62b4ce5c4cb5d970aa57693228eae840e
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-05-10 21:47:09 +0530

    Add Views and URL patterns

--- travelassistant/urls.py
+++ travelassistant/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from core import views
 
 
 urlpatterns = [
@@ -9,5 +11,6 @@ urlpatterns = [
     path('todo/', include('todo.urls')),
     path('albums/', include('albums.urls')),
     path('users/', include('users.urls')),
-    path('admin/', admin.site.urls),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
